ec6240f74eb37acbd057bdfdef9b9eaeebdcbca5
kkata <14849535+kkata@users.noreply.github.com>
2020-12-02 16:55:52 +0900

    :hammer: impliment start and gameover

--- src/App.js
+++ src/App.js
@@ -9,6 +9,20 @@ const initialPosition = { x: 17, y: 17 }
 const initialValue = initFields(35, initialPosition)
 const defaultInterval = 100
 
+const GameStatus = Object.freeze({
+  init: 'init',
+  playing: 'playing',
+  suspended: 'suspended',
+  gameover: 'gameover',
+})
+
+const Direction = Object.freeze({
+  up: 'up',
+  right: 'right',
+  left: 'left',
+  down: 'down',
+})
+
 let timer = undefined
 
 const unsubscribe = () => {
@@ -18,9 +32,21 @@ const unsubscribe = () => {
   clearInterval(timer)
 }
 
+const isCollision = (fieldSize, position) => {
+  if (position.y < 0 || position.x < 0) {
+    return true
+  }
+  if (position.y > fieldSize - 1 || position.x > fieldSize - 1) {
+    return true
+  }
+  return false
+}
+
 function App() {
   const [fields, setFields] = useState(initialValue)
   const [position, setPosition] = useState()
+  const [status, setStatus] = useState(GameStatus.init)
+  const [direction, setDirection] = useState(Direction.up)
   const [tick, setTick] = useState(0)
 
   useEffect(() => {
@@ -32,19 +58,39 @@ function App() {
   }, [])
 
   useEffect(() => {
-    if (!position) {
+    if (!position || status !== GameStatus.playing) {
       return
     }
-    goUp()
+    const canContinue = goUp()
+    if (!canContinue) {
+      setStatus(GameStatus.gameover)
+    }
   }, [tick])
+
+  const onStart = () => setStatus(GameStatus.playing)
+
+  const onRestart = () => {
+    timer = setInterval(() => {
+      setTick((tick) => tick + 1)
+    }, defaultInterval)
+    setDirection(Direction.up)
+    setStatus(GameStatus.init)
+    setPosition(initialPosition)
+    setFields(initFields(35, initialPosition))
+  }
 
   const goUp = () => {
     const { x, y } = position
-    const nextY = Math.max(y - 1, 0)
+    const newPosition = { x, y: y - 1 }
+    if (isCollision(fields.length, newPosition)) {
+      unsubscribe()
+      return false
+    }
     fields[y][x] = ''
-    fields[nextY][x] = 'snake'
-    setPosition({ x, y: nextY })
+    fields[newPosition.y][x] = 'snake'
+    setPosition(newPosition)
     setFields(fields)
+    return true
   }
 
   return (
@@ -59,7 +105,7 @@ function App() {
         <Field fields={fields} />
       </main>
       <footer className="footer">
-        <Button />
+        <Button status={status} onStart={onStart} onRestart={onRestart} />
         <ManipulationPanel />
       </footer>
     </div>
