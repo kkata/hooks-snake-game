e39d07061e5798b60b809a22d58bc74eafd0f334
kkata <14849535+kkata@users.noreply.github.com>
2020-12-02 13:50:16 +0900

    :package: use uuid

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const Field = ({ fields }) => {
   return (
     <div className="field">
       {fields.map((row) => {
         return row.map((column) => {
-          return <div className={`dots ${column}`}></div>
+          return <div className={`dots ${column}`} key={uuidv4()}></div>
         })
       })}
     </div>
