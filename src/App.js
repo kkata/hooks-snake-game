fe907deef737c50cab6428b1a63877573cffc37b
kkata <14849535+kkata@users.noreply.github.com>
2020-12-03 14:18:05 +0900

    :hammer: implement eating food

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navigation from './components/Navigation'
 import Field from './components/Field'
 import Button from './components/Button'
 import ManipulationPanel from './components/ManipulationPanel'
-import { initFields } from './utils'
+import { initFields, getFoodPosition } from './utils'
 
 const initialPosition = { x: 17, y: 17 }
 const initialValue = initFields(35, initialPosition)
@@ -65,13 +65,13 @@ const isCollision = (fieldSize, position) => {
 
 function App() {
   const [fields, setFields] = useState(initialValue)
-  const [position, setPosition] = useState()
+  const [body, setBody] = useState([])
   const [status, setStatus] = useState(GameStatus.init)
   const [direction, setDirection] = useState(Direction.up)
   const [tick, setTick] = useState(0)
 
   useEffect(() => {
-    setPosition(initialPosition)
+    setBody([initialPosition])
     timer = setInterval(() => {
       setTick((tick) => tick + 1)
     }, defaultInterval)
@@ -79,7 +79,7 @@ function App() {
   }, [])
 
   useEffect(() => {
-    if (!position || status !== GameStatus.playing) {
+    if (body.length === 0 || status !== GameStatus.playing) {
       return
     }
     const canContinue = handleMoving()
@@ -96,7 +96,7 @@ function App() {
     }, defaultInterval)
     setDirection(Direction.up)
     setStatus(GameStatus.init)
-    setPosition(initialPosition)
+    setBody(initialPosition)
     setFields(initFields(35, initialPosition))
   }
 
@@ -126,15 +126,23 @@ function App() {
   }, [onChangeDirection])
 
   const handleMoving = () => {
-    const { x, y } = position
+    const { x, y } = body[0]
     const delta = Delta[direction]
     const newPosition = { x: x + delta.x, y: y + delta.y }
     if (isCollision(fields.length, newPosition)) {
       return false
     }
-    fields[y][x] = ''
+    const newBody = [...body]
+    if (fields[newPosition.y][newPosition.x] !== 'food') {
+      const removingTrack = newBody.pop()
+      fields[removingTrack.y][removingTrack.x] = ''
+    } else {
+      const food = getFoodPosition(fields.length, [...newBody, newPosition])
+      fields[food.y][food.x] = 'food'
+    }
     fields[newPosition.y][newPosition.x] = 'snake'
-    setPosition(newPosition)
+    newBody.unshift(newPosition)
+    setBody(newBody)
     setFields(fields)
     return true
   }
